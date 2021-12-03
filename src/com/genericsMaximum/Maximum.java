7bcb71df81c14f6fe8e2a33c1f6cc6038bf8aceb
memories2013 <nshivaprasad41@gmail.com>
2021-12-03 12:50:43 +0530

    Maximum String Value

--- src/com/genericsMaximum/Maximum.java
+++ src/com/genericsMaximum/Maximum.java
@@ -1,32 +1,13 @@
 package com.genericsMaximum;
 
-public class Maximum <T extends Comparable<T>> {
-
-	T first;
-	T second;
-	T third;
-	
-	
-
-	public static <T extends Comparable<T>> void findMax(T first, T second, T third) {
-		if(first.compareTo(second) >= 0 && first.compareTo(third) >= 0) {
-			System.out.println(" First is largest ");
+public class Maximum {
+	public void maxString(String first,String second,String third) {
+		if(first.compareTo(second) >=0 && first.compareTo(third) >=0) {
+			System.out.println("First Element is Largest");
+		}else if(second.compareTo(first) >= 0 && third.compareTo(third) >=0) {
+			System.out.println("Second Element is Largest");
+		}else {
+			System.out.println("Third Element is Largest");
 		}
-		else if(second.compareTo(first) >= 0 && second.compareTo(third) >= 0) {
-			System.out.println(" Second is largest");
-		}
-		else
-			System.out.println(" Third is largest");
-	}
-	
-	public void findMax() {
-		findMax(this.first, this.second, this.third);
-	}
-	
-	public Maximum(T first, T second, T third) {
-		super();
-		this.first = first;
-		this.second = second;
-		this.third = third;
 	}
 }
--- src/com/genericsMaximum/TestMaximum.java
+++ src/com/genericsMaximum/TestMaximum.java
@@ -4,62 +4,19 @@ import java.util.Scanner;
 
 public class TestMaximum {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Maximum max = new Maximum();
 		Scanner scan = new Scanner(System.in);
-		Maximum max;
 		
-		while(true) {
-			System.out.println("1.Integers\n2.Float\n3.String\nSelect Your Choice: ");
-			int ch = scan.nextInt();
-			
-			switch(ch) {
-			case 1:
-			{
-				System.out.print("Enter First Value: ");
-				Integer first = scan.nextInt();
+		System.out.print("Enter First Value: ");
+		String first = scan.next();
+		
+		System.out.print("Enter Second Value: ");
+		String second = scan.next();
+		
+		System.out.print("Enter Third Value: ");
+		String third = scan.next();
+		
+		max.maxString(first,second,third);
 				
-				System.out.print("Enter Second Value: ");
-				Integer second = scan.nextInt();
-				
-				System.out.print("Enter Third Value: ");
-				Integer third = scan.nextInt();
-				max = new Maximum<Integer>(first, second, third);
-				max.findMax();
-				break;
-			}				
-			case 2:
-			{
-				System.out.print("Enter First Value: ");
-				Float first = scan.nextFloat();
-				
-				System.out.print("Enter Second Value: ");
-				Float second = scan.nextFloat();
-				
-				System.out.print("Enter Third Value: ");
-				Float third = scan.nextFloat();
-				max = new Maximum<Float>(first, second, third);
-				max.findMax();
-				break;
-			}
-			case 3:
-			{
-				System.out.print("Enter First Value: ");
-				String first = scan.next();
-				
-				System.out.print("Enter Second Value: ");
-				String second = scan.next();
-				
-				System.out.print("Enter Third value ");
-				String third = scan.next();
-				max = new Maximum<String>(first, second, third);
-				max.findMax();
-				break;
-			}
-			default:
-				System.out.println("Enter Choice Between 1 And 3");				
-			}
-				
-		}
 	}
-
 }
